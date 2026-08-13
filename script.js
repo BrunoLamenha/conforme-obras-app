ee17ae610a519d6fd933625d7d6fe3dedeea5a2b
BrunoLamenha <lamenhabruno@gmail.com>
2026-08-13 08:21:44 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-
+function navegar(destino) {
+    if (destino === 'vistoria') {
+        alert("Redirecionando para o fluxo de Vistoria...");
+        // Próximo passo: carregar a lista de empreendimentos estruturais e arquitetônicos
+    } else if (destino === 'cadastros') {
+        alert("Redirecionando para o fluxo de Cadastros...");
+        // Próximo passo: escolher da lista ou cadastrar novo empreendimento (Wizard)
+    }
+}
